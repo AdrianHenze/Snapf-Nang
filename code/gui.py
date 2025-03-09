290db6fc3ac76b5449a764718cf154a0f1d78fba
Adrian Henze <74675007+AdrianHenze@users.noreply.github.com>
2025-03-09 10:17:47 +0100

    GUI Button Page Changes

--- code/gui.py
+++ code/gui.py
@@ -3,32 +3,43 @@ import tkinter as tk
 from PIL import Image, ImageTk
 from code.widgets import *
 
-# Window
-root = tk.Tk()
-root.title("Snapf-Nang Bank")
+root = None
+window_width = None
+main_frame = None
+l_page = None
+r_page_width = None
+l_page_width = None
+l_page = None
 
-# Window Size and Location
-screen_width = root.winfo_screenwidth()
-screen_height = root.winfo_screenheight()
-window_width = int(screen_width * 0.69)
-window_height = int(screen_height * 0.69)
-window_x = (screen_width - window_width) // 2
-window_y = (screen_height - window_height) // 2
-root.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
-
-
-def build_frame1():
-    # Set Up
-    frame1 = tk.Frame(root, bg=color_gray)
-    frame1.pack(fill="both", expand=True)
+def build_window():
+    global root, window_width, r_page_width, l_page_width
+    root = tk.Tk()
+    root.title("Snapf-Nang Bank")
+    # Size and Location
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+    window_width = int(screen_width * 0.69)
+    window_height = int(screen_height * 0.69)
+    window_x = (screen_width - window_width) // 2
+    window_y = (screen_height - window_height) // 2
+    root.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
+    # Sections
     r_page_width = 600
     l_page_width = window_width - r_page_width
-    frame1.grid_rowconfigure(0, weight=1)
-    frame1.grid_columnconfigure(0, weight=2)
-    frame1.grid_columnconfigure(1, weight=2, minsize=r_page_width)
+    return root
 
-    def build_right_page():
-        r_page = tk.Frame(frame1, bg=color_space_gray, width=r_page_width)
+
+def build_main_frame():
+    global main_frame, l_page
+    # Set Up
+    main_frame = tk.Frame(root, bg=color_gray)
+    main_frame.pack(fill="both", expand=True)
+    main_frame.grid_rowconfigure(0, weight=1)
+    main_frame.grid_columnconfigure(0, weight=2)
+    main_frame.grid_columnconfigure(1, weight=2, minsize=r_page_width)
+
+    def build_right_navigation():
+        r_page = tk.Frame(main_frame, bg=color_space_gray, width=r_page_width)
         r_page.grid(row=0, column=1, sticky="nsew")
         r_page.grid_propagate(False)
         # Frame to center Buttons on Right Side
@@ -38,18 +49,17 @@ def build_frame1():
         nav_frame.grid_rowconfigure(4, weight=1)
         nav_frame.grid_columnconfigure(0, weight=1)
         # Buttons
-        def button_test_function(text):
-            print(text)
         def button(frame, row, col, text, command):
-            b = ttk.Button(frame, text=text, style="Custom.TButton", command=lambda: command(text))
+            b = ttk.Button(frame, text=text, style="Custom.TButton", command=command)
             b.grid(row=row, column=col, sticky="ew", pady=10, padx=10)
         style = button_style(root)
-        button(nav_frame, 0, 0, "Deposit", button_test_function)
-        button(nav_frame, 3, 0, "Withdrawal", button_test_function)
-        button(nav_frame, 4, 0, "Balance", button_test_function)
+        button(nav_frame, 0, 0, "Deposit", open_deposit_page)
+        button(nav_frame, 3, 0, "Withdrawal", open_withdrawal_page)
+        button(nav_frame, 4, 0, "Balance", open_balance_page)
 
-    def build_left_page():
-        l_page = tk.Frame(frame1, bg=color_gray, width=l_page_width)
+    def build_left_title_page():
+        global l_page
+        l_page = tk.Frame(main_frame, bg=color_gray, width=l_page_width)
         l_page.grid(row=0, column=0, sticky="nsew")
         l_page.grid_propagate(False)
         # Image
@@ -61,10 +71,53 @@ def build_frame1():
         img.image = img_file
         img.place(relx=0.5, rely=0.5, anchor="center")
     
-    build_right_page()
-    build_left_page()
+    build_right_navigation()
+    build_left_title_page()
+
+
+def open_deposit_page():
+    global l_page
+    l_page.destroy()  # Remove Current Left Page
+
+    deposit_page = tk.Frame(main_frame, bg=color_gray, width=l_page_width)
+    deposit_page.grid(row=0, column=0, sticky="nsew")
+    deposit_page.grid_propagate(False)
+
+    label = tk.Label(deposit_page, text="Deposit Page", bg=color_gray, fg=color_white)
+    label.place(relx=0.5, rely=0.5, anchor="center")
+    
+    l_page = deposit_page  # Update Global Reference
+
+
+def open_withdrawal_page():
+    global l_page
+    l_page.destroy()  # Remove Current Left Page
+
+    withdrawal_page = tk.Frame(main_frame, bg=color_gray, width=l_page_width)
+    withdrawal_page.grid(row=0, column=0, sticky="nsew")
+    withdrawal_page.grid_propagate(False)
+
+    label = tk.Label(withdrawal_page, text="Withdrawal Page", bg=color_gray, fg=color_white)
+    label.place(relx=0.5, rely=0.5, anchor="center")
+    
+    l_page = withdrawal_page  # Update Global Reference
+
+
+def open_balance_page():
+    global l_page
+    l_page.destroy()  # Remove Current Left Page
+
+    balance_page = tk.Frame(main_frame, bg=color_gray, width=l_page_width)
+    balance_page.grid(row=0, column=0, sticky="nsew")
+    balance_page.grid_propagate(False)
+
+    label = tk.Label(balance_page, text="Balance Page", bg=color_gray, fg=color_white)
+    label.place(relx=0.5, rely=0.5, anchor="center")
+    
+    l_page = balance_page  # Update Global Reference
 
 
 def start_snapf_nang():
-    build_frame1()
+    build_window()
+    build_main_frame()
     tk.mainloop()
