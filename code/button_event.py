c60ba5613e6fa23bd19b99384606134428676515
Adrian Henze <74675007+AdrianHenze@users.noreply.github.com>
2025-03-10 22:30:21 +0100

    Backend-Frontend Connection

--- code/button_event.py
+++ code/button_event.py
@@ -1,13 +1,20 @@
+from code.backend import *
+
 def is_valid_login(name, passw):
-    if name == "Banane" and passw == "1234":
-        print("✅ Login successful.")
-        return True
-    else:
-        print("❌ ACCESS DENIED: Wrong name or password.\n> Try Banane and 1234.")
+    rc = login(name, passw)
+    if rc == -1 or rc == -2:
+        print("❌ ACCESS DENIED.")
+        return rc
+    print("✅ Login successful.")
+    return True
 
 
 def is_valid_registration(name, passw, year):
     if name and passw and year:
+        rc = konto_erstellen(name, passw)
+        if rc == -1:
+            print("❌ REGISTRATION FAILED: Name is already in use.")
+            return False
         print("✅ Registration successful.")
         return True
     else:
@@ -15,11 +22,12 @@ def is_valid_registration(name, passw, year):
 
 
 def get_balance_if():
-    return "1.000.000,00"
+    return get_kontostand()
 
 
 def deposit_if(amount):
     if amount:
+        einzahlen(amount)
         print(f"✅ Deposit of {amount} € successful.")
     else:
         print("❌ DEPOSIT AMOUNT MISSING.")
@@ -27,19 +35,28 @@ def deposit_if(amount):
 
 def withdrawal_if(amount):
     if amount:
-        print(f"✅ Withdrawal of {amount} € successful.")
+        rc = abheben(amount)
+        if rc == -1:
+            print("❌ WITHDRAWAL DENIED: Not enough money in the bank.")
+        else:
+            print(f"✅ Withdrawal of {amount} € successful.")
     else:
         print("❌ WITHDRAWAL AMOUNT MISSING.")
 
 
 def transfer_if(name, amount, ref):
     if name and amount:
-        print(f"✅ Transfer of {amount} € to {name} successful.")
-        if ref:
-            print(f"> Reference: {ref}")
+        rc = ueberweisung(name, amount)
+        if rc == -1:
+            print("❌ TRANSFER DENIED: Not enough money in the bank.")
+        else:
+            print(f"✅ Transfer of {amount} € to {name} successful.")
+            if ref:
+                print(f"> Reference: {ref}")
     else:
         print("❌ TRANSFER INFORMATION MISSING.")
 
 
 def logout_if():
+    logout()
     print("✅ Logout successful.")
