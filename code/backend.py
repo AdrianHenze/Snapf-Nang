a5c350572358a9b5ea1ce045b31d6af94bc8a8d8
Snapf <190218887+St-Sven@users.noreply.github.com>
2025-03-10 17:50:48 +0100

    weitere funktionen

--- code/backend.py
+++ code/backend.py
@@ -63,5 +63,45 @@ class Bank:
         self.konten = {}
         self.angemeldetes_konto = None
 
-    def konto_erstellung(self, inhaber, passwort, geburtsjahr)
-        
+    def konto_erstellung(self, inhaber, passwort, geburtsjahr):
+        if inhaber in self.konten:
+            raise ValueError("Fehler: Konto mit diesem Inhalber existiert bereits.")
+        neues_konto = Konto(inhaber, passwort, geburtsjahr)
+        self.konten[inhaber] = neues_konto
+
+    def login(self, inhaber, passwort):
+        if inhaber not in self.konten:
+            raise ValueError("Fehler: Konto mit diesem Inhaber existiert nicht.")
+        konto = self.konto[inhaber]
+        if konto.passwort != passwort:
+            raise ValueError("Fehler: Passwort ist falsch.")
+        self.angemeldetes_konto = konto
+        return f"Willkommen, {inhaber}!"
+    
+    def logout(self):
+        if self.angemeldetes_konto is None:
+            raise ValueError("Fehler: Konto bereits eingeloggt.")
+        self.angemeldetes_konto = None
+        return "Aufwiedersehen."
+    
+    def ueberweisung(self, ziel_inhaber, betrag):
+        if ziel_inhaber is not self.konten:
+            raise ValueError("Fehler: Zielkonto existiert nicht.")
+        if self.angemeldetes_konto.kontostand < betrag:
+            raise ValueError("Fehler: Nicht genug guthaben für diese Überweisung.")
+        ziel_konto = self.konten[ziel_inhaber]
+        self.angemeldetes_konto.kontostand -= betrag
+        ziel_konto.kontostand += betrag
+        datum = datetime.now().strftime("%d/&m/%Y, %H:%M:%S")
+        self.angemeldetes_konto.transaktionen.append({
+            "typ": "Überweisung gesendet"
+            "betrag": betrag,
+            "datum": datum,
+            "quelle": ziel_inhaber
+        })
+        ziel_konto.transaktion.append({
+            "typ": "Überweisung empfangen",
+            "betrag": betrag,
+            "datum": datum,
+            "quelle": self.angemeldetes_konto.inhaber
+        })
